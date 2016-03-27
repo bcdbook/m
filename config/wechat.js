0884cf17de5e280b314ef10dcd3ee37ed62c3f8b
lixian13149999 <lixian13149999@163.com>
2016-03-27 14:55:50 +0800

    13

--- config/wechat.js
+++ config/wechat.js
@@ -42,9 +42,13 @@ var API = require('wechat-api');
 var api = new API(appid, appsecret);
 
 // 微信下方栏目的开发配置开始==============
-api.createMenu(menus, function(err, result) {
-  console.log(result);
-});
+module.exports = function() {
+
+  api.createMenu(menus, function(err, result) {
+    console.log(result);
+  });
+}
+
 // 微信下方栏目的开发配置结束==============
 // 微信下方栏目的开发配置结束==============
 
